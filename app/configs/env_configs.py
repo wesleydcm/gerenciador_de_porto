725787ca9420f4ff67d1998a3c168ddbbd2ee6f2
Marcelo Romão <mcabralromao@gmail.com>
2021-10-14 19:33:28 -0300

    fix: env_configs - adding production config

--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -7,8 +7,12 @@ load_dotenv()
 
 def init_app(app:Flask):
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = bool(os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'))
     app.config['JSON_SORT_KEYS'] = bool(os.environ.get('JSON_SORT_KEYS'))
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+    if os.environ.get('FLASK_ENV') == 'production': 
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
