3877cfb936683e9402bdec65033dc9967708b025
Felipe <adfelipealmeida@gmail.com>
2021-10-15 00:51:33 -0300

    fix: alter any tables

--- app/models/container_model.py
+++ app/models/container_model.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.schema import ForeignKey
 from app.configs.database import db
 from sqlalchemy import Column, Integer, String
 from dataclasses import dataclass
@@ -9,11 +10,15 @@ class ContainerModel (db.Model):
     codigo_rastreio: int
     teu: int
     tipo: str
-    
+
     __tablename__ = 'containers'
 
     id_container = Column(Integer, primary_key=True)
-    codigo_rastreio = Column(String(100), nullable=False, unique=True)
+    codigo_rastreio = Column(Integer, nullable=False, unique=True)
     teu = Column(Integer, nullable=False, default=1)
     tipo = Column(String(255), nullable=False, default='dry box')
-    
+    id_empresa_maritima = Column(
+        Integer, ForeignKey(
+            "empresa_maritima.id_empresa_maritima", ondelete="cascade"
+        )
+    )
