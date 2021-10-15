58fd7d738d7461ef0d17104798b0d60eb25528ee
wesleydcm <wesleycostamatos@gmail.com>
2021-10-15 04:12:05 -0300

    feat: add return when container not found

--- app/controllers/container_controller.py
+++ app/controllers/container_controller.py
@@ -6,6 +6,9 @@ from dataclasses import asdict
 def get_one_container(id:int):
 
     container = ContainerModel.query.get(id)
+
+    if not container:
+        return {'msg': 'container not found'}, 404
     
     return jsonify(container), 200
 
