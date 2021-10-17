fe73ef7f2e2442d932151bcf99184bc3550e0118
Felipe <adfelipealmeida@gmail.com>
2021-10-17 17:05:18 -0300

    feat: route delete company is working

--- app/controllers/company_controller.py
+++ app/controllers/company_controller.py
@@ -2,15 +2,12 @@ from flask import jsonify, request, current_app
 from http import HTTPStatus
 from datetime import datetime
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from app.exceptions.comapny_erros import CompanyNotFound
 
 from app.models.company_model import ShippingCompany
 from app.models.container_model import Container
 from app.controllers.user_controller import session
 from app.models.company_model import ShippingCompany
 from app.models.user_model import User
-
-import pdb
 
 
 @jwt_required()
@@ -66,6 +63,20 @@ def update(trading_name: str):
         return {"Error": "Company not found!"}, HTTPStatus.BAD_REQUEST
 
 
+@jwt_required()
+def delete(trading_name: str):
+    try:
+        user_data = get_jwt_identity()
+        user = User.query.filter_by(username=user_data["username"]).first()
+
+        for company in user.company:
+                if company.trading_name == trading_name:
+                    current_company = company
+
+        session(current_company, "remove")
+        return {"trading_name": current_company.trading_name}, HTTPStatus.OK
+    except UnboundLocalError:
+        return {"Error": "Company not found!"}, HTTPStatus.BAD_REQUEST
 
 
 
--- app/routes/company_blueprint.py
+++ app/routes/company_blueprint.py
@@ -1,7 +1,7 @@
 from sys import path
 from flask import Blueprint
 from app.controllers.company_controller import (
-    get_company, register_company, update
+    get_company, register_company, update, delete
 )
 
 bp = Blueprint('company_bp', __name__, url_prefix='/shipping_company')
@@ -9,3 +9,4 @@ bp = Blueprint('company_bp', __name__, url_prefix='/shipping_company')
 bp.post("")(register_company)
 bp.get("<string:trading_name>")(get_company)
 bp.patch("<string:trading_name>")(update)
+bp.delete("<string:trading_name>")(delete)
