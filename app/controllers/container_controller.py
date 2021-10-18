1bca7836a8acda0eca3d7ac0edf01dfdfbb1f1ab
Paulo Mello <rapha.mello87@gmail.com>
2021-10-18 12:38:36 -0300

    feat: travels of container started to be implemented

--- app/controllers/container_controller.py
+++ app/controllers/container_controller.py
@@ -1,9 +1,13 @@
 from http import HTTPStatus
-from flask_jwt_extended.utils import get_jwt_identity
-from flask_jwt_extended.view_decorators import jwt_required
+
+import sqlalchemy
 from app.models.company_model import ShippingCompany
 from app.models.container_model import Container
+from app.models.user_model import User
 from flask import current_app, jsonify, request
+from flask_jwt_extended.utils import get_jwt_identity
+from flask_jwt_extended.view_decorators import jwt_required
+from psycopg2.errors import NotNullViolation, UniqueViolation
 
 
 def list_containers():
@@ -22,79 +26,120 @@ def get_one_container(id_container: int):
     return jsonify(container), 200
 
 
-@jwt_required
+@jwt_required()
 def create_container():
-    user = get_jwt_identity()
+    user_from_jwt = get_jwt_identity()
+    user = User.query.filter_by(username=user_from_jwt['username']).first()
+
     data = request.json
 
-    shipping_company = ShippingCompany.query.filter_by(id_user=user['id']).first()
+    shipping_company = ShippingCompany.query\
+        .filter_by(id_user=user.id_user).first()
 
-    data['id_shipping_company'] = shipping_company.id
+    data['id_shipping_company'] = shipping_company.id_shipping_company
 
-    new_container = Container(**data)
+    try:
+        new_container = Container(**data)
 
-    current_app.db.session.add(new_container)
-    current_app.db.session.commit()
+        current_app.db.session.add(new_container)
+        current_app.db.session.commit()
 
-    return jsonify(new_container), HTTPStatus.CREATED
+        return jsonify(new_container), HTTPStatus.CREATED
+
+    except sqlalchemy.exc.IntegrityError as e:
+        if type(e.orig) == NotNullViolation:
+            return {'msg': 'Tracking code is required'}, HTTPStatus.BAD_REQUEST
+        if type(e.orig) == UniqueViolation:
+            return {'msg': 'Tracking code already registered'}, HTTPStatus.BAD_REQUEST
 
 
-@jwt_required
+@jwt_required()
 def get_container_by_tracking_code(tracking_code: int):
-    user = get_jwt_identity()
+    user_from_jwt = get_jwt_identity()
+    user = User.query.filter_by(username=user_from_jwt['username']).first()
 
-    container = Container.query.filter_by(tracking_code=tracking_code)
+    shipping_company = ShippingCompany.query\
+        .filter_by(id_user=user.id_user).first()
 
-    shipping_company = ShippingCompany.query.filter_by(id_user=user['id']).first()
-
+    container = Container.query.filter_by(tracking_code=tracking_code).first()
+    
     if not container:
         return {'msg': 'Container not found'}, HTTPStatus.NOT_FOUND
 
-    if shipping_company['id'] != container['id_shipping_company']:
+    if shipping_company.id_shipping_company != container.id_shipping_company:
         return {'msg': f'This tracking code {tracking_code} does not belong to your company'}, HTTPStatus.BAD_REQUEST
 
     return jsonify(container), HTTPStatus.OK
 
 
-@jwt_required
+@jwt_required()
 def update_container_by_tracking_code(tracking_code: int):
-    user = get_jwt_identity()
+    user_from_jwt = get_jwt_identity()
+    user = User.query.filter_by(username=user_from_jwt['username']).first()
+
     data = request.json
 
-    container = Container.query.filter_by(tracking_code=tracking_code)
+    container = Container.query.filter_by(tracking_code=tracking_code).first()
 
-    shipping_company = ShippingCompany.query.filter_by(id_user=user['id']).first()
+    shipping_company = ShippingCompany.query\
+        .filter_by(id_user=user.id_user).first()
 
     if not container:
         return {'msg': 'Container not found'}, HTTPStatus.NOT_FOUND
 
-    if shipping_company['id'] != container['id_shipping_company']:
+    if shipping_company.id_shipping_company != container.id_shipping_company:
         return {'msg': f'This tracking code {tracking_code} does not belong to your company'}, HTTPStatus.BAD_REQUEST
     try:
         Container.query.filter_by(tracking_code=tracking_code).update(data)
 
-        updated_container = Container.query.get(tracking_code=tracking_code)
+        updated_container = Container.query\
+            .filter_by(tracking_code=tracking_code)\
+            .first()
 
         return jsonify(updated_container), HTTPStatus.OK
-    except TypeError:  # TODO: mudar o tipo do erro para um mais específico
+
+    except sqlalchemy.exc.InvalidRequestError:
         return {'msg': 'check the data to be updated'}, HTTPStatus.BAD_REQUEST
 
 
-@jwt_required
+@jwt_required()
 def delete_container_by_tracking_code(tracking_code: int):
-    user = get_jwt_identity()
+    user_from_jwt = get_jwt_identity()
+    user = User.query.filter_by(username=user_from_jwt['username']).first()
 
-    container = Container.query.filter_by(tracking_code=tracking_code)
+    container = Container.query.filter_by(tracking_code=tracking_code).first()
 
     if not container:
         return {'msg': 'Container not found'}, HTTPStatus.NOT_FOUND
 
-    shipping_company = ShippingCompany.query.filter_by(id_user=user['id']).first()
+    shipping_company = ShippingCompany.query\
+        .filter_by(id_user=user.id_user)\
+        .first()
 
-    if shipping_company['id'] != container['id_shipping_company']:
+    if shipping_company.id_shipping_company != container.id_shipping_company:
         return {'msg': f'This tracking code {tracking_code} does not belong to your company'}, HTTPStatus.BAD_REQUEST
 
     current_app.db.session.delete(container)
     current_app.db.session.commit()
 
     return {}, HTTPStatus.NO_CONTENT
+
+
+@jwt_required()
+def get_travels_of_container(tracking_code: int):
+    user_from_jwt = get_jwt_identity()
+    user = User.query.filter_by(username=user_from_jwt['username']).first()
+
+    shipping_company = ShippingCompany.query\
+        .filter_by(id_user=user.id_user)\
+        .first()
+
+    container = Container.query.filter_by(tracking_code=tracking_code).first()
+
+    if not container:
+        return {'msg': 'Container not found'}, HTTPStatus.NOT_FOUND
+
+    if shipping_company.id_shipping_company != container.id_shipping_company:
+        return {'msg': f'This tracking code {tracking_code} does not belong to your company'}, HTTPStatus.BAD_REQUEST
+
+    return jsonify(container.travels), HTTPStatus.OK
--- app/routes/container_blueprint.py
+++ app/routes/container_blueprint.py
@@ -1,7 +1,12 @@
-from app.controllers.container_controller import get_one_container, list_containers
+from app.controllers.container_controller import create_container, delete_container_by_tracking_code, get_container_by_tracking_code, get_travels_of_container, update_container_by_tracking_code
 from flask import Blueprint
 
 bp = Blueprint('container_bp', __name__, url_prefix='/container')
 
-bp.get('')(list_containers)
-bp.get('/<int:id_container>')(get_one_container)
+# bp.get('')(list_containers)
+# bp.get('/<int:id_container>')(get_one_container)
+bp.post('')(create_container)
+bp.get('/<int:tracking_code>')(get_container_by_tracking_code)
+bp.patch('/<int:tracking_code>')(update_container_by_tracking_code)
+bp.delete('/<int:tracking_code>')(delete_container_by_tracking_code)
+bp.get('/<int:tracking_code>/travels')(get_travels_of_container)
