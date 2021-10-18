01bc31d8ca4681c2d3cdaf1014bd78996420e163
Felipe <adfelipealmeida@gmail.com>
2021-10-18 10:53:43 -0300

    feat: routes reposrts company is working

--- app/controllers/company_controller.py
+++ app/controllers/company_controller.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.models.company_model import ShippingCompany
-from app.models.container_model import Container
 from app.controllers.user_controller import session
 from app.models.company_model import ShippingCompany
 from app.models.user_model import User
@@ -79,15 +78,33 @@ def delete(trading_name: str):
         return {"Error": "Company not found!"}, HTTPStatus.BAD_REQUEST
 
 
+@jwt_required()
+def list_containers_by_company(trading_name: str):
+    try:
+        user_data = get_jwt_identity()
+        user = User.query.filter_by(username=user_data["username"]).first()
+
+        for company in user.company:
+            if company.trading_name == trading_name:
+                current_company = company
+
+        return jsonify(current_company.containers), HTTPStatus.OK
+
+    except UnboundLocalError:
+        return {"Error": "Company not found!"}, HTTPStatus.BAD_REQUEST
 
 
-def list_containers_by_company(id: int):
+@jwt_required()
+def list_ships_by_company(trading_name: str):
+    try:
+        user_data = get_jwt_identity()
+        user = User.query.filter_by(username=user_data["username"]).first()
 
-    company: ShippingCompany = ShippingCompany.query.get(id)
+        for company in user.company:
+            if company.trading_name == trading_name:
+                current_company = company
 
-    containers = Container.query.filter_by(id_shipping_company = company.id_shipping_company).all()
+        return jsonify(current_company.ships), HTTPStatus.OK
 
-    if not containers:
-        return {'msg': "company does not have registered containers"}, HTTPStatus.NOT_FOUND
-
-    return jsonify(containers), HTTPStatus.OK
+    except UnboundLocalError:
+        return {"Error": "Company not found!"}, HTTPStatus.BAD_REQUEST
--- app/routes/company_blueprint.py
+++ app/routes/company_blueprint.py
@@ -1,7 +1,8 @@
 from sys import path
 from flask import Blueprint
 from app.controllers.company_controller import (
-    get_company, register_company, update, delete
+    get_company, register_company, update, delete,
+    list_containers_by_company, list_ships_by_company
 )
 
 bp = Blueprint('company_bp', __name__, url_prefix='/shipping_company')
@@ -10,3 +11,6 @@ bp.post("")(register_company)
 bp.get("<string:trading_name>")(get_company)
 bp.patch("<string:trading_name>")(update)
 bp.delete("<string:trading_name>")(delete)
+
+bp.get("<string:trading_name>/containers")(list_containers_by_company)
+bp.get("<string:trading_name>/ships")(list_ships_by_company)
