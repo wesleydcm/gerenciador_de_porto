649aeccf8038afcf960fe2d17fbfa1b65d29d686
Humberto <matheushumberto520@gmail.com>
2021-10-19 15:11:45 -0400

    check credentials of user.

--- app/controllers/ship_controller.py
+++ app/controllers/ship_controller.py
@@ -3,14 +3,33 @@ from http import HTTPStatus
 from app.controllers.utils import session
 from app.models.ship_model import Ship
 from flask import jsonify, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from app.models.company_model import ShippingCompany
+from app.models.user_model import User
 
 
 @jwt_required()
 def create_ship():
     try:
+        user_from_jwt = get_jwt_identity()
+        user = User.query.filter_by(username=user_from_jwt['username']).first()
+
         data = request.json
+        companyName = data.pop("company")
+        company = ShippingCompany.query.filter_by(
+            trading_name=companyName
+            ).first()
+        if not company:
+            return {"msg": "Company not found."}, HTTPStatus.NOT_FOUND
+
+        if company.id_user != user.id_user:
+            return {
+                "msg": "User does not have credentials of this company"
+                }, HTTPStatus.UNAUTHORIZED
+
+        data["id_shipping_company"] = company.id_shipping_company
+
         new_ship = Ship(**data)
         session(new_ship, "add")
         return jsonify(new_ship), HTTPStatus.CREATED
